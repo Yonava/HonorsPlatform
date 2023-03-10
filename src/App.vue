0638960df2e2eff695ba4bda1bcb4a8d48a228c4
yonava <yonavossandreae@gmail.com>
2023-03-10 13:18:44 -0800

    vuetify layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,42 @@
 <template>
-  <div></div>
+  <v-app>
+    <v-app-bar
+      color="blue-darken-2"
+      class="px-5"
+    >
+      <div class="d-flex align-center">
+        <v-icon 
+          icon="mdi-account-group" 
+          size="x-large" 
+          class="mr-2"
+        ></v-icon>
+        <h1 style="font-weight: 700;">
+          Students
+        </h1>
+        <p class="ml-2">
+          ({{ students.length }})
+        </p>
+      </div>
+    </v-app-bar>
+    <v-main>
+
+    </v-main>
+  </v-app>
 </template>
 
-<style scoped>
+<script setup>
+import { ref } from 'vue'
 
-</style>
+const students = ref([
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@snhu.edu',
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    email: 'jane'
+  },
+])
+</script>
