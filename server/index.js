ad2012b92dd572858b7b66420331fca13fb69a51
yonava <yonavossandreae@gmail.com>
2023-03-16 01:31:43 -0700

    crud

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const { google } = require('googleapis');
+const spreadsheetId = '1bW-aQRn-GAbTsNkV2VB9xtBFT3n-LPrSJXua_NA2G6Y';
 
-async function getSpreadsheetData() {
+async function createSheet() {
   // config for google api service account
   const config = {
     keyFilename: path.join(process.cwd(), 'credentials.json'),
@@ -11,13 +12,33 @@ async function getSpreadsheetData() {
   const auth = await google.auth.getClient(config);
 
   const sheets = google.sheets({ version: 'v4', auth });
+  return sheets;
+}
 
+async function getSpreadsheetData(sheets) {
   const response = await sheets.spreadsheets.values.get({
-    spreadsheetId: '1bW-aQRn-GAbTsNkV2VB9xtBFT3n-LPrSJXua_NA2G6Y',
+    spreadsheetId,
     range: 'Students',
   });
 
   console.log(response.data.values);
 }
 
-getSpreadsheetData();
+async function deleteStudent(sheets, row) {
+  const response = await sheets.spreadsheets.values.update({
+    spreadsheetId,
+    range: `Students!A${row}:Z${row}`,
+    valueInputOption: 'USER_ENTERED',
+    resource: {
+      values: [[
+        '', '', ''
+      ]]
+    }
+  });
+  console.log(response.data);
+}
+
+createSheet().then((sheets) => {
+  // getSpreadsheetData(sheets);
+  deleteStudent(sheets, 4);
+});
