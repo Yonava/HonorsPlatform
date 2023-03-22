7bea79098d1e47117fda77c0892d23e9863ded84
yonava <yonavossandreae@gmail.com>
2023-03-22 00:57:53 -0400

    added nice loading to auth

--- client/src/views/AuthPage.vue
+++ client/src/views/AuthPage.vue
@@ -5,11 +5,19 @@
     width="100vw"
     color="blue-lighten-3"
   >
+    <v-progress-circular
+      v-if="loading"
+      indeterminate
+      color="blue-darken-2"
+      size="64"
+    ></v-progress-circular>
     <img
+      v-if="!loading"
       src="../assets/honorsLogo.png"
-      style="margin-bottom: 10px; border-radius: 5px; width: 300px; height: 200px; object-fit: cover; mix-blend-mode: multiply;"
+      class="logo mb-7"
     />
     <v-sheet 
+      v-if="!loading"
       color="blue-darken-2"
       class="d-flex align-center justify-center flex-column pa-5"
       style="border-radius: 10px;"
@@ -17,7 +25,6 @@
       <h2 class="mb-3">
         Click to attempt authorization
       </h2>
-      
       <v-btn @click="$router.push('/panel')">
         Authorize
       </v-btn>
@@ -31,9 +38,22 @@ import { onMounted, ref } from 'vue'
 
 const route = useRoute()
 const router = useRouter()
+const loading = ref(true)
 
 onMounted(() => {
   localStorage.setItem('token', route.query.code as string)
   router.push({ name: 'panel' })
+  setTimeout(() => {
+    loading.value = false
+  }, 2000)
 })
 </script>
+
+<style scoped>
+.logo {
+  width: 300px;
+  height: 200px;
+  object-fit: cover;
+  mix-blend-mode: multiply;
+}
+</style>
