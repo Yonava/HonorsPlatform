8e6769e3e3580f6145baadc9266e76964d0ce4e1
yonava <yonavossandreae@gmail.com>
2023-03-22 00:49:34 -0400

    added decorative elements to auth

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -34,11 +34,10 @@ const router = createRouter({
 })
 
 router.beforeEach(async (to, from) => {
-  if (to.name === 'auth' || to.name === 'points') return
-  const token = localStorage.getItem('token') || 'token'
+  if (to.name === 'auth') return
+  const token = localStorage.getItem('token') ?? ''
   const res = await fetch(`/api/auth/${encodeURIComponent(token)}`)
   const data = await res.json()
-  console.log(data)
   if (data.url) location.replace(data.url)
 })
 
--- client/src/views/AuthPage.vue
+++ client/src/views/AuthPage.vue
@@ -1,8 +1,28 @@
 <template>
-  <div>
-    auth
-    <v-btn @click="$router.push('/panel')">retry</v-btn>
-  </div>
+  <v-sheet 
+    class="d-flex align-center justify-center flex-column"
+    height="100vh"
+    width="100vw"
+    color="blue-lighten-3"
+  >
+    <img
+      src="../assets/honorsLogo.png"
+      style="margin-bottom: 10px; border-radius: 5px; width: 300px; height: 200px; object-fit: cover; mix-blend-mode: multiply;"
+    />
+    <v-sheet 
+      color="blue-darken-2"
+      class="d-flex align-center justify-center flex-column pa-5"
+      style="border-radius: 10px;"
+    >
+      <h2 class="mb-3">
+        Click to attempt authorization
+      </h2>
+      
+      <v-btn @click="$router.push('/panel')">
+        Authorize
+      </v-btn>
+    </v-sheet>
+  </v-sheet>
 </template>
 
 <script setup lang="ts">
@@ -13,9 +33,6 @@ const route = useRoute()
 const router = useRouter()
 
 onMounted(() => {
-  if (!route.query.code) {
-    return
-  }
   localStorage.setItem('token', route.query.code as string)
   router.push({ name: 'panel' })
 })
