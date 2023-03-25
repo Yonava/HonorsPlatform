f1b0fbff6c540df7d875675cea77d8619dab11fa
yonava <yonavossandreae@gmail.com>
2023-03-25 00:30:19 -0400

    style header and loader on announcement

--- client/src/components/AnnouncementMenu.vue
+++ client/src/components/AnnouncementMenu.vue
@@ -14,18 +14,46 @@
 
     <v-list 
       width="600"
-      class="pa-3"
+      class="pa-5"
     >
-      <h2 style="line-height: 0.8">
-        Announcements From The Desk of Dr. Matthews:
-      </h2>
+      <div class="d-flex align-center">
+        <v-icon 
+          size="x-large"
+          class="mr-3"
+        >mdi-bullhorn-variant</v-icon>
+        <h1 style="line-height: 0.8">
+          Announcements
+        </h1>
+      </div>
+      <div 
+        v-if="loading"
+        class="d-flex justify-center"
+      >
+        <v-progress-circular 
+          size="32"
+          indeterminate
+          class="ma-10"
+        ></v-progress-circular>
+      </div>
+      <div 
+        v-else-if="announcements.length === 0"
+        class="d-flex justify-center"
+      >
+        <h2 
+          class="ma-10"
+          style="color: green"
+        >
+          <v-icon class="mr-1 mb-1">mdi-check</v-icon>
+          No current announcements
+        </h2>
+      </div>
       <v-list-item
         v-for="announcement in announcements"
         :key="announcement"
-        class="announcement pa-3 mt-3"
         :style="{
           background: `rgba(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255}, 0.2)`
         }"
+        class="announcement pa-3 mt-3"
       >
         {{ announcement }}
       </v-list-item>
@@ -34,25 +62,32 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { getEvery } from '../SheetsAPI'
 
 const announcements = ref([])
+const loading = ref(true)
 const active = ref(false)
 
-const fetchAnnouncements = async () => {
-  const res = await getEvery('Announcements')
-  announcements.value = res.map((row: any) => row[0])
-    .filter((row: any) => row)
-}
+watch(active, async (val) => {
+  if (val) {
+    await fetchAnnouncements()
+  }
+})
 
-fetchAnnouncements()
+const fetchAnnouncements = async () => {
+  loading.value = true
+  announcements.value = []
+  const res = await getEvery('Announcements')
+  announcements.value = res.map((row: string[]) => row[0])
+    .filter((announcement: string) => announcement)
+  loading.value = false
+}
 </script>
 
 <style scoped>
 .announcement {
   font-size: 1.2rem;
-  line-height: 1;
   border-radius: 5px;
 }
 </style>
