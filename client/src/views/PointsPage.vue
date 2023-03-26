ee2708939616c16472e0466d0a3e189536946290
yonava <yonavossandreae@gmail.com>
2023-03-26 16:37:52 -0400

    leaderboard styling

--- client/src/views/PointsPage.vue
+++ client/src/views/PointsPage.vue
@@ -1,19 +1,69 @@
 <template>
-  <div>
-    <v-sheet class="d-flex align-center justify-center flex-column">
-      <h1>
-        Honors Leaderboard:
+  <div class="outer-matte">
+    <v-sheet 
+      class="d-flex align-center justify-center flex-column"
+      style="width: 90%; height: 90%;"
+      elevation="10"
+    >
+      <h1 
+        style="font-size: 3rem; font-weight: 700;"
+      >
+        Honors Leaderboard
       </h1>
-
       <div 
         v-if="!error && !loading"
-        class="d-flex flex-column"
+        class="d-flex flex-column align-center"
       >
+        <div style="width: 95%" class="d-flex justify-start">
+          <h2>Rankings</h2>
+        </div>
+        <div class="d-flex flex-row justify-space-between mb-3">
+          <v-sheet
+            v-for="(rank, index) in ['yellow-darken-1', 'grey-darken-1', 'brown-darken-2']"
+            :key="rank"
+            :color="rank"
+            class="mx-3 pa-2"
+            style="border-radius: 10px; border: 1px solid black;"
+            elevation="4"
+          > 
+            <h2>{{ students[index].name }}</h2>
+            <h4>#{{ index + 1 }} with {{ students[index].points }} points</h4>
+          </v-sheet>
+        </div>
+        <div style="border-bottom: 1px solid black; width: 100%;"></div>
         <div 
-          v-for="student in students"
-          :key="student.name"
+          class="d-flex flex-column align-center justify-space-between"
+          style="height: 500px; width: 100%; overflow: auto"
         >
-          {{ student.name }}: {{ student.points }}
+          <div 
+            v-for="student in students.slice(3)"
+            :key="student.name"
+            style="width: 60%;"
+          >
+            <div 
+              class="d-flex flex-row align-center justify-space-between"
+              style="width: 100%;"
+            >
+              <div 
+                style="width: 25%;"
+                class="d-flex flex-row justify-start"
+              >
+                <div>{{ student.rank }}.</div>
+              </div>
+              <div 
+                style="width: 50%"
+                class="d-flex flex-row justify-center"
+              >
+                <div>{{ student.name }}</div>
+              </div>
+              <div 
+                style="width: 25%"
+                class="d-flex flex-row justify-end"
+              >
+                <div>{{ student.points }}</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
       <div v-else-if="error">
@@ -38,16 +88,31 @@ const error = ref(false);
 const loading = ref(true);
 
 async function getStudents() {
-  let data;
   try {
-    data = await getNonSensitiveData('points');
+    const data = await getNonSensitiveData('points');
+    students.value = data
+      .sort((a, b) => b.points - a.points)
+      .map((student, index) => ({
+        name: student.name,
+        points: student.points.toLocaleString(),
+        rank: index + 1,
+      }))
   } catch {
     error.value = true;
   }
-  students.value = data;
-  students.value.sort((a, b) => b.points - a.points);
   loading.value = false;
 }
 
 getStudents();
 </script>
+
+<style scoped>
+.outer-matte {
+  background-color: #EBB02D;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
