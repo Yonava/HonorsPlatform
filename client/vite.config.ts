f450fda56c32efe1665c92bb32690fd848b7043d
yonava <yonavossandreae@gmail.com>
2023-04-05 14:43:37 -0400

    hit heroku

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,28 +1,43 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const defaultConfig = {
+export default defineConfig({
   plugins: [vue()],
-  port: 5177,
   build: {
-    outDir: '../server/public',
+    outDir: '../server/public'
   },
-}
-
-export default defineConfig(({ command, mode }) => {
-  const devEnv = mode === 'development'
-  const target = devEnv ? 'http://localhost:1010' : 'https://honors.herokuapp.com'
-  return {
-    ...defaultConfig,
-    server: {
-      proxy: {
-        '/api': {
-          target: 'https://honors.herokuapp.com',
-          changeOrigin: devEnv,
-          secure: !devEnv,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://honors.herokuapp.com',
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
-    },
+    }
   }
 })
+
+// const defaultConfig = {
+//   plugins: [vue()],
+//   port: 5177,
+//   build: {
+//     outDir: '../server/public',
+//   },
+// }
+
+// export default defineConfig(({ command, mode }) => {
+//   const devEnv = mode === 'development'
+//   const target = devEnv ? 'http://localhost:1010' : 'https://honors.herokuapp.com'
+//   return {
+//     ...defaultConfig,
+//     server: {
+//       proxy: {
+//         '/api': {
+//           target: 'https://honors.herokuapp.com',
+//           changeOrigin: devEnv,
+//           secure: !devEnv,
+//           rewrite: (path) => path.replace(/^\/api/, '')
+//         }
+//       }
+//     },
+//   }
+// })
