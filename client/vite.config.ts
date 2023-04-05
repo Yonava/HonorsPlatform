779ebb86b1e3223d0eaa6512c85bfba082ef4ae8
yonava <yonavossandreae@gmail.com>
2023-04-05 14:08:05 -0400

    changeOrigin off

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'http://localhost:1010',
-        changeOrigin: true,
+        // changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
