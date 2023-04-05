37b726bd26cb9ae5d7b6e984f34134db361e7093
yonava <yonavossandreae@gmail.com>
2023-04-05 14:21:27 -0400

    devEnv config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const defaultConfig = {
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:1010',
-        // changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
+  port: 5177,
   build: {
     outDir: '../server/public',
+  },
+}
+
+export default defineConfig(({ command, mode }) => {
+  const devEnv = mode === 'development'
+  const target = devEnv ? 'http://localhost:1010' : 'https://honors.herokuapp.com'
+  console.log(devEnv)
+  return {
+    ...defaultConfig,
+    server: {
+      proxy: {
+        '/api': {
+          target,
+          changeOrigin: devEnv,
+          secure: !devEnv,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
   }
 })
