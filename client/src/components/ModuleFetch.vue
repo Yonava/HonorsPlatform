89254f51a67f18a0a0ceb47bac8b230e0c869725
yonava <yonavossandreae@gmail.com>
2023-04-14 00:54:14 -0400

    modules display undergoing major refactor

--- client/src/components/ModuleFetch.vue
+++ client/src/components/ModuleFetch.vue
@@ -1,86 +1,140 @@
 <template>
   <div>
-    <ModuleList
-      v-if="!loadingModules"
-      @delete="reqDeleteModule($event)"
-      @update="reqUpdateModule($event)"
-      :modules="modules"
+    <LockArea 
+      v-if="!id"
+      title="Module Tracking"
+      condition="student ID"
     />
-    <div 
-      v-else
-      class="d-flex justify-center my-5"
-    >
-      <v-progress-circular 
-        indeterminate
-        color="blue"
-      ></v-progress-circular>
+    <div v-else>
+      <div class="d-flex flex-row align-center">
+        <h2>
+          Modules In Progress:
+        </h2>
+        <v-spacer></v-spacer>
+        <v-btn
+          @click="showModuleAddModal = true"
+          size="small"
+          color="green"
+        >
+          <v-icon class="mr-1">mdi-plus</v-icon>
+          Add Module
+        </v-btn>
+      </div>
+      <div
+        v-if="loading"
+        class="d-flex flex-row justify-center"
+      >
+        <v-progress-circular
+          indeterminate
+          color="purple-darken-2"
+        ></v-progress-circular>
+      </div>
+      <div
+        v-else-if="engagements.length === 0"
+        style="font-weight: 200; color: red; font-size: 25px"
+      >
+        No events yet.
+      </div>
+      <div
+        v-else
+        style="max-height: 500px; overflow: auto"
+      >
+        <ModuleList
+          v-if="!loadingModules"
+          @delete="reqDeleteModule($event)"
+          @update="reqUpdateModule($event)"
+          :modules="modules"
+        />
+      </div>
+      <ModuleDetailModal 
+        @close="closeModal"
+        @add="addEngagementEvent($event)"
+        @update="updateEngagementEvent($event)"
+        :item="selectedEngagement"
+        :show="showModal"
+      />
+      <AddModal 
+        @close="showAddModal = false"
+        @success="fetch"
+        :show="showAddModal"
+        :panel="switchPanel(PanelType.MODULES)"
+        :override="{
+          color: 'blue',
+          predefineColumnData: [id],
+        }"
+      />
     </div>
   </div>
+
 </template>
 
 <script setup lang="ts">
-import { 
-  ref, 
-  watch,
-  computed,
-} from 'vue'
+import { ref, computed, watch } from 'vue'
+import ModuleList from './ModuleList.vue'
+import LockArea from './LockArea.vue'
+import { Module } from '../SheetTypes'
+import ModuleDetailModal from './ModuleDetailModal.vue'
+import { mapModules, unmapModules } from '../DataMappers'
 import { 
   getEvery, 
   clearByRow, 
-  updateByRow,
-  Range,
+  postInRange, 
+  Range, 
+  updateByRow 
 } from '../SheetsAPI'
-import ModuleList from '../components/ModuleList.vue'
-import { mapModules, unmapModules } from '../DataMappers'
-import { Module } from '../SheetTypes'
-
-const modules = ref<Module[]>([])
-const loadingModules = ref(false)
 
 const props = defineProps<{
-  studentId: string
-  refetch: boolean
+  id: string | undefined
 }>()
 
-const emits = defineEmits([
-  'toggleCanDelete',
-])
+const clone = <T>(obj: T): T => JSON.parse(JSON.stringify(obj))
 
-watch(() => props.studentId, async () => {
-  await fetchModules()
+const emits = defineEmits([
+  'update', 
+  'loading-state'
+])
+const loading = ref(true)
+const modules = ref<Module[]>([])
+const selectedModule = ref<Module | undefined>(undefined)
+const showModal = ref(false)
+const showAddModal = ref(false)
+
+watch(loading, (val) => {
+  emits('loading-state', val)
+})
+
+watch(() => props.id , async () => {
+  await fetch()
 }, { immediate: true })
 
-watch(() => props.refetch, async () => {
-  await fetchModules()
-})
-
-async function fetchModules() {
-  loadingModules.value = true
-  const unmappedModules = await getEvery(Range.MODULES)
-  modules.value = mapModules(unmappedModules)
-    .filter(module => module.studentId === props.studentId)
-  loadingModules.value = false
+async function fetch() {
+  loading.value = true
+  modules.value = []
+  const events = await getEvery(Range.MODULES)
+  modules.value = mapModules(events).filter(e => e.studentId === props.id)
+  emits('update', modules.value)
+  loading.value = false
 }
 
-const canBeDeleted = computed(() => {
-  return modules.value.length === 0 && !loadingModules.value
-})
-
-watch(canBeDeleted, (val) => {
-  emits('toggleCanDelete', val)
-})
-
-async function reqDeleteModule(row) {
-  loadingModules.value = true
-  await clearByRow(Range.MODULES, row)
-  await fetchModules()
+async function updateModule(event: Module) {
+  loading.value = true
+  await updateByRow(Range.MODULES, event.row, unmapModules([event]))
+  await fetch()
 }
 
-async function reqUpdateModule(module) {
-  loadingModules.value = true
-  if (module) {
-    await updateByRow(Range.MODULES, module.row, unmapModules([module]))
-  }
-  await fetchModules()
+async function deleteModule(event: Module) {
+  loading.value = true
+  await clearByRow(Range.MODULES, event.row)
+  await fetch()
+}
+
+function openModal(selected: Module) {
+  selectedModule.value = selected
+  showModal.value = true
+}
+
+function closeModal() {
+  showModal.value = false
+  selectedModule.value = undefined
 }
 </script>
