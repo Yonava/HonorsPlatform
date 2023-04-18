aba8d11e0c844580b5a7dc2ff1a62358fc98b193
yonava <yonavossandreae@gmail.com>
2023-04-17 21:04:10 -0400

    final reactivity syncing refactor

--- client/src/SheetItemUpdateManager.ts
+++ client/src/SheetItemUpdateManager.ts
@@ -2,23 +2,73 @@ import { updateByRow } from "./SheetsAPI";
 import type { Ref } from "vue";
 import type { SheetItem } from "./SheetTypes";
 import type { Panel } from "./SwitchPanel";
-import { watch } from "vue";
+import { watch, provide, ref, onUnmounted } from "vue";
+
+export type SyncState = {
+  status: boolean;
+  processing: boolean;
+  lastSynced: Date;
+}
 
 export function useUpdateManager(selectedItemRef: Ref<SheetItem>, panel: Ref<Panel<SheetItem>>) {
 
-  async function updateItem(item: SheetItem) {
+  const syncState = ref<SyncState>({
+    status: false,
+    processing: false,
+    lastSynced: new Date()
+  });
+  provide('syncState', syncState);
+
+  let startingItem = '';
+  let switchedStudentProfile = false;
+
+  async function updateItem() {
+    if (syncState.value.processing || !selectedItemRef.value) return;
+    syncState.value.processing = true;
     await updateByRow(
       panel.value.sheetRange, 
-      item.row, 
+      selectedItemRef.value.row, 
       await panel.value.mappers.unmap([
-        item
+        selectedItemRef.value
       ])
     )
+    startingItem = JSON.stringify(selectedItemRef.value);
+    syncState.value = {
+      status: true,
+      processing: false,
+      lastSynced: new Date()
+    }
   }
 
   watch(selectedItemRef, async (newItem, oldItem) => {
-    if (!oldItem) return;
-    await updateItem(oldItem);
+    if (!oldItem) {
+      startingItem = JSON.stringify(newItem);
+      return;
+    };
+    if (startingItem !== JSON.stringify(oldItem))  {
+      await updateByRow(
+        panel.value.sheetRange, 
+        oldItem.row, 
+        await panel.value.mappers.unmap([
+          oldItem
+        ])
+      )
+    }
+    if (!newItem) return;
+    startingItem = JSON.stringify(newItem);
   });
-  
+
+  watch(selectedItemRef, async (newItem, oldItem) => {
+    switchedStudentProfile = newItem !== oldItem;
+    syncState.value.status = false;
+  }, { deep: true });
+
+  const syncInterval = setInterval(async () => {
+    if (syncState.value.status || switchedStudentProfile) return;
+    await updateItem();
+  }, 3500);
+
+  onUnmounted(() => {
+    clearInterval(syncInterval);
+  });
 }
--- client/src/components/UpdateButton.vue
+++ client/src/components/UpdateButton.vue
@@ -1,72 +1,24 @@
 <template>
   <v-btn 
     icon
-    @click="reqUpdate"
-    :loading="loading"
-    :color="upToDate ? 'green' : `${panel.color}-darken-2`"
+    :loading="syncState.processing"
+    :color="syncState.status ? 'green' : `red-darken-2`"
     :style="{
-      cursor: upToDate ? 'default' : 'pointer',
+      cursor: syncState.status ? 'default' : 'pointer',
     }"
     rounded
     class="ml-7"
   >
     <v-icon
-      :icon="upToDate ? 'mdi-check' : 'mdi-cloud-upload'"
+      :icon="syncState.status ? 'mdi-check' : 'mdi-cloud-upload'"
     ></v-icon>
   </v-btn>
 </template>
 
 <script setup lang="ts">
-import { Panel } from "../SwitchPanel";
-import { SheetItem } from "../SheetTypes";
-import { updateByRow } from "../SheetsAPI";
-import { ref, inject, computed, toRefs, watch } from "vue";
-import type { Ref } from 'vue'
-import { useAutoSync } from '../AutoSync'
+import { inject } from "vue";
+import type { Ref } from "vue";
+import type { SyncState } from "../SheetItemUpdateManager";
 
-const clone = <T>(obj: T): T => JSON.parse(JSON.stringify(obj))
-
-const props = defineProps<{
-  item: SheetItem
-}>()
-
-const { item } = toRefs(props)
-
-useAutoSync(reqUpdate)
-
-const panel = inject("activePanel") as Ref<Panel<SheetItem>>
-
-const emits = defineEmits([
-  'updated'
-]);
-
-const loading = ref(false);
-const upToDate = ref(false);
-const cooldown = ref(false);
-
-async function reqUpdate() {
-  if (upToDate.value) return
-  upToDate.value = true
-  cooldown.value = true
-  // TODO: this reactivity system is a mess, refactor!!!!
-  setTimeout(() => cooldown.value = false, 1000)
-  loading.value = true
-  await updateByRow(
-    panel.value.sheetRange, 
-    props.item.row, 
-    await panel.value.mappers.unmap([
-      props.item
-    ])
-  )
-  emits('updated', clone(props.item))
-  loading.value = false
-}
-
-watch(item,
-  () => {
-    if (cooldown.value) return
-    upToDate.value = false
-  }, 
-  { deep: true }
-)
+const syncState = inject<Ref<SyncState>>("syncState");
 </script>
