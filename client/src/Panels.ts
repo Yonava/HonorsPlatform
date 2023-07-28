b0b168667d6dad8365063ba16baa2ad7b900bf12
yonava <yona@vossandreae.com>
2023-07-28 23:22:35 +0200

    v0.88 t2

--- client/src/Panels.ts
+++ client/src/Panels.ts
@@ -224,4 +224,4 @@ export type GetSheetItemType<T extends PanelRange> = T extends 'Students' ? type
 
 export type Panel = typeof panels[PanelName];
 export const getPanel = (panelName: PanelName) => panels[panelName];
-export const version = 'prerelease v0.88.T (beta)'
+export const version = 'prerelease v0.88.T2 (beta)'
