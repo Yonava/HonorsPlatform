593c4ca923f11b9a8318d74ac864045d0096ab43
yonava <yona@vossandreae.com>
2024-01-27 00:32:04 -0800

    better autocomplete featurs

--- client/src/composables/useAutoComplete.ts
+++ client/src/composables/useAutoComplete.ts
@@ -1,32 +1,39 @@
 import { toRef, computed } from "vue"
 import type { MaybeRefOrGetter, Ref } from "vue"
 import { useDocumentCache } from "@store/useDocumentCache"
-import type { IncludeByProp, SheetItemKeys, JointSheetItem } from "@apptypes/sheetItems"
+import type { IncludeByProp, SheetItemKeys } from "@apptypes/sheetItems"
 
 const getAllItemsWithProperty = <T extends SheetItemKeys>(prop: T) => {
   const { allSheetItems: items } = useDocumentCache()
   return items.filter((item) => {
     return prop in item
-  }) as IncludeByProp<T, JointSheetItem>[]
+  }) as IncludeByProp<T>[]
 }
 
-export function useAutoComplete<T extends string, K extends SheetItemKeys>(
-  propSource: K,
-  userInput?: MaybeRefOrGetter<T>,
+const autoCompleteButton = <T>(input: string | undefined, suggestedValues: T[]) => {
+  const suggestion = suggestedValues[0]
+  const showButton = suggestion && input !== suggestion
+  return {
+    condition: showButton,
+    newPropValue: () => suggestion,
+    text: suggestion,
+  } as const
+}
+
+export function useAutoComplete<T>(
+  source: T[],
+  userInput?: MaybeRefOrGetter<string>,
 ) {
-  const items = getAllItemsWithProperty(propSource)
   const possibleValues = [
     ...new Set(
-      items
-        .map((item) => item[propSource])
-        .filter((value) => !!value?.toString().trim())
+      source.filter((value) => !!value?.toString().trim())
     )
   ]
   const input = toRef(userInput)
 
   const suggestedValues = computed(() => {
-    const inputString = input.value?.toString().toLowerCase() || ""
     const str = (v: any) => v?.toString().toLowerCase() || ""
+    const inputString = str(input.value)
 
     const doesInclude = (v: any) => str(v).includes(inputString)
     const prioritizeStartsWith = (a: any, b: any) => {
@@ -42,24 +49,18 @@ export function useAutoComplete<T extends string, K extends SheetItemKeys>(
       .sort(prioritizeStartsWith)
   })
 
+  const button = computed(() => autoCompleteButton(input.value, suggestedValues.value))
+
   return {
+    button,
     suggestedValues,
     possibleValues,
-    input
-  }
+    input,
+  } as const
 }
 
 export const useInstructorAutoComplete = <T extends string>(inputSrc: Ref<T>) => {
-  const autoComplete = useAutoComplete("instructor", inputSrc)
-  const suggestion = computed(() => autoComplete.suggestedValues.value[0])
-  const suggestionAvailable = computed(() => autoComplete.suggestedValues.value.length > 0)
-  const showButton = computed(() => suggestionAvailable.value && inputSrc.value !== suggestion.value)
-  return {
-    autoComplete,
-    button: computed(() => ({
-      condition: showButton.value || !inputSrc.value,
-      newPropValue: () => suggestion.value,
-      text: suggestion.value,
-    }))
-  }
+  const instructors = getAllItemsWithProperty("instructor").map(item => item.instructor)
+  const mentors = getAllItemsWithProperty("mentor").map(item => item.mentor)
+  return useAutoComplete([...instructors, ...mentors], inputSrc)
 }
